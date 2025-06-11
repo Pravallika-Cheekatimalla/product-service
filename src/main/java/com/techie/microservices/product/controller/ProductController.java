fe8a9638bab3312e204ef75b95cea5d5b92d4604
Pravallika-Cheekatimalla <cheekatimalla.pravallika@gmail.com>
2025-06-11 12:53:53 -0400

    Initial Version

--- src/main/java/com/techie/microservices/product/controller/ProductController.java
+++ src/main/java/com/techie/microservices/product/controller/ProductController.java
@@ -2,9 +2,9 @@ package com.techie.microservices.product.controller;
 
 import com.techie.microservices.product.dto.ProductRequest;
 import com.techie.microservices.product.dto.ProductResponse;
-import com.techie.microservices.product.model.Product;
 import com.techie.microservices.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,18 +12,24 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
-@RequiredArgsConstructor
 
 public class ProductController {
     private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ProductResponse createProduct(@RequestBody ProductRequest productRequest){
-       return productService.createProduct(productRequest);
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
     }
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<ProductResponse> getAllProducts(){
+    public List<ProductResponse> getAllProducts() {
         return productService.getAllProducts();
     }
 }
